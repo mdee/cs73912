e6edc106330c83f8d61d99dd9555837f74598f8d
Mark Dhillon <mark.dhillon@gmail.com>
2012-11-26 17:21:03 -0600

    Adding missing ReplicantService file

---
+++ src/main/java/com/cs739/app/service/master/ReplicantService.java
@@ -0,0 +1,35 @@
+package com.cs739.app.service.master;
+
+import java.util.List;
+
+import com.cs739.app.model.Replicant;
+import com.cs739.app.util.AppConstants;
+
+/**
+ * Manages {@link Replicant} instances for the Master
+ * @author MDee
+ *
+ */
+public class ReplicantService {
+    
+    /**
+     * The client wants to upload a file, this method picks a destination.
+     * @param replicants - list of {@link Replicant} instances available
+     * @return a single {@link Replicant}
+     */
+    public static Replicant chooseReplicantForUpload(List<Replicant> replicants) {
+        // Very simple right now, simply chooses the first one that's available
+        Replicant replicant = null;
+        for (Replicant r : replicants) {
+            if (r.isAvailable()) {
+                replicant = r;
+            }
+        }
+        return replicant;
+    }
+
+    public static String generateReplicantId(int id) {
+        return AppConstants.REPLICANT_ID_PREFIX + id;
+    }
+    
+}
